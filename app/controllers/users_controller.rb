8141c7ad8c13720e7129b8a8a1d8f439dcdd005f
Suneil Basu <suneil@futureprogress.com>
2016-05-04 23:03:49 +0100

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to :action => 'index'
+    else
+      redirect_to :action => 'index'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :member)
+  end
+end
